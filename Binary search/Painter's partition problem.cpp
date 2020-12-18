109ac8d3fa082a9b84ffff41626bac8398168fa2
Vishwam36 <58394139+Vishwam36@users.noreply.github.com>
2020-12-18 18:53:44 +0530

    Update Painter's partition problem.cpp

--- Binary search/Painter's partition problem.cpp
+++ Binary search/Painter's partition problem.cpp
@@ -21,3 +21,47 @@ bool can(int A, int B, vector<int> &C, int mid)
     }
     return true;
 }
+
+long long binary(int A, int B, vector<int> &C, int max, int sum)
+{
+    long long l=0,r=sum,mid=0;
+    while(r-l>1)
+    {
+        mid=(l+r)/2;
+        bool temp = can(A,B,C,mid);
+        if(temp)
+        {
+            r=mid;
+        }
+        else
+        {
+            l=mid+1;
+        }
+    }
+    if(can(A,B,C,l))
+    {
+        return l%10000003;
+    }
+    return r%10000003;
+}
+
+int Solution::paint(int A, int B, vector<int> &C) {
+    long long n=C.size(),max=0,sum=0;
+    for(long long i=0;i<n;++i)
+    {
+        C[i]%=10000003;
+        sum+=C[i];
+        sum=sum%10000003;
+        if(C[i]>max)
+        {
+            max=C[i];
+        }
+    }
+    if(A>C.size())
+    {
+        return max*(B%10000003);
+    }
+    long long ans = binary(A,B,C,max,sum)*(B%10000003);
+    ans = ans%10000003;
+    return (int)ans;
+}
