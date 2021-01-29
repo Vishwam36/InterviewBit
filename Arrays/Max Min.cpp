fd008db7210090ced91f74a1fb586f2e66efbcd3
Vishwam36 <58394139+Vishwam36@users.noreply.github.com>
2021-01-29 21:40:08 +0530

    Create Max Min.cpp

---
+++ Arrays/Max Min.cpp
@@ -0,0 +1,10 @@
+// By Vishwam Shriram Mundada
+// https://www.interviewbit.com/problems/max-min-05542f2f-69aa-4253-9cc7-84eb7bf739c4/
+
+int Solution::solve(vector<int> &A) 
+{
+    int max = *max_element(A.begin(), A.end());
+    int min = *min_element(A.begin(), A.end());
+    
+    return min + max;
+}
