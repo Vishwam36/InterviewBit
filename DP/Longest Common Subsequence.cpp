6f3d30738d5b8573677b4ada2b18442795d56fbe
Vishwam Shriram Mundada <58394139+Vishwam36@users.noreply.github.com>
2021-02-12 19:23:53 +0530

    Create Longest Common Subsequence.cpp

---
+++ DP/Longest Common Subsequence.cpp
@@ -0,0 +1,25 @@
+// By Vishwam Shriram Mundada
+// https://www.interviewbit.com/problems/longest-common-subsequence/
+// Simple but important
+
+int Solution::solve(string A, string B) 
+{
+    int n = A.size(), m = B.size();
+    int dp[n+1][m+1];
+
+    for(int i = 0; i <= n; ++i)
+    {
+        for(int j = 0; j <= m; ++j)
+        {
+            if(i == 0 || j == 0)
+                dp[i][j] = 0;
+            
+            else if(A[i-1] == B[j-1])
+                dp[i][j] = 1 + dp[i-1][j-1];
+            
+            else
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+        }
+    }
+    return dp[n][m];
+}
