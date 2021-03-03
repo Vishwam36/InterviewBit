d17792e2f8fae00beda32fe46e947aaf72b06390
Vishwam Shriram Mundada <58394139+Vishwam36@users.noreply.github.com>
2021-03-03 17:09:18 +0530

    Create Kth Row of Pascal's Triangle.cpp

---
+++ Arrays/Kth Row of Pascal's Triangle.cpp
@@ -0,0 +1,17 @@
+// By Vishwam Shriram Mundada
+// https://www.interviewbit.com/problems/kth-row-of-pascals-triangle/
+// Easy, just take new concept
+
+vector<int> Solution::getRow(int A) 
+{
+    vector<int> ans;
+    int n = 1;
+    
+    for(int i = 0; i <= A; ++i)
+    {
+        ans.push_back(n);
+        n = (n * (A-i))/(i+1);
+    }
+    
+    return ans;
+}
