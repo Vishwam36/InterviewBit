1242e8d7c88c9c47666c469620cbbef2a6f43df4
Vishwam Shriram Mundada <58394139+Vishwam36@users.noreply.github.com>
2021-03-07 19:35:18 +0530

    Create Remove Half Nodes.cpp

---
+++ Tree Data Structure/Remove Half Nodes.cpp
@@ -0,0 +1,33 @@
+// By Vishwam Shriram Mundada
+// https://www.interviewbit.com/problems/remove-half-nodes/
+// Tricky
+
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+TreeNode* Solution::solve(TreeNode* root)
+{
+    if(root == NULL)
+        return NULL;
+    
+    root -> left = solve(root -> left);
+    root -> right = solve(root -> right);
+    
+    if(root -> left == NULL && root -> right == NULL)
+        return root;
+    
+    if(root -> left == NULL)
+        return root -> right;
+        
+    if(root -> right == NULL)
+        return root -> left;
+        
+    return root;
+}
