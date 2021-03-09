fe32ab60b6774242a9ae8797160c0ca28513b1b8
Vishwam Shriram Mundada <58394139+Vishwam36@users.noreply.github.com>
2021-03-09 16:29:40 +0530

    Create Postorder Traversal.cpp

---
+++ Tree Data Structure/Postorder Traversal.cpp
@@ -0,0 +1,58 @@
+// By Vishwam Shriram Mundada
+// https://www.interviewbit.com/problems/postorder-traversal/
+// Good
+
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+/**
+ * use stack of pair
+ * pair = {node, visit status}
+ * if status = 0 then both left and right children are not visited
+ * if status = 1 then left child is visited
+ * if status = 2 then both are visited
+*/
+
+vector<int> Solution::postorderTraversal(TreeNode* root) 
+{
+    vector<int> ans;
+    
+    if(root == NULL)
+        return ans;
+        
+    stack<pair<TreeNode*, int> > s;
+    s.push({root,0});
+    
+    TreeNode* curr;
+
+    while(!s.empty())
+    {
+        curr = s.top().first;
+        
+        if(s.top().second == 0)
+        {
+            s.top().second++;
+            if(curr -> left != NULL)
+                s.push({curr -> left, 0});
+        }
+        else if(s.top().second == 1)
+        {
+            s.top().second++;
+            if(curr -> right != NULL)
+                s.push({curr -> right, 0});
+        }
+        else
+        {
+            ans.push_back(curr -> val);
+            s.pop();
+        }
+    }
+    return ans;
+}
