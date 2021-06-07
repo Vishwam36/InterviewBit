f91a5f5242b0dce8643724d77ca87c65287e44d7
Vishwam Shriram Mundada <58394139+Vishwam36@users.noreply.github.com>
2021-06-07 13:03:06 +0530

    Good question, Standard

--- Binary search/Painter's partition problem.cpp
+++ Binary search/Painter's partition problem.cpp
@@ -1,67 +1,111 @@
-// By Vishwam Mundada
-// Written function only which was asked
+// By Vishwam Shriram Mundada
+// https://www.interviewbit.com/problems/painters-partition-problem/
+// Good problem
 
-bool can(int A, int B, vector<int> &C, int mid)
+/*
+Given 2 integers A and B and an array of integars C of size N.
+
+Element C[i] represents length of ith board.
+
+You have to paint all N boards [C0, C1, C2, C3 … CN-1]. There are A painters available and each of them takes B units of time to paint 1 unit of board.
+
+Calculate and return minimum time required to paint all boards under the constraints that any painter will only paint contiguous sections of board.
+
+2 painters cannot share a board to paint. That is to say, a board
+cannot be painted partially by one painter, and partially by another.
+A painter will only paint contiguous boards. Which means a
+configuration where painter 1 paints board 1 and 3 but not 2 is
+invalid.
+Return the ans % 10000003
+
+Input Format
+
+The first argument given is the integer A.
+The second argument given is the integer B.
+The third argument given is the integer array C.
+Output Format
+
+Return minimum time required to paint all boards under the constraints that any painter will only paint contiguous sections of board % 10000003.
+Constraints
+
+1 <=A <= 1000
+1 <= B <= 10^6
+1 <= C.size() <= 10^5
+1 <= C[i] <= 10^6
+For Example
+
+Input 1:
+    A = 2
+    B = 5
+    C = [1, 10]
+Output 1:
+    50
+Explanation 1:
+    Possibility 1:- same painter paints both blocks, time taken = 55units
+    Possibility 2:- Painter 1 paints block 1, painter 2 paints block 2, time take = max(5, 50) = 50
+    There are no other distinct ways to paint boards.
+    ans = 50%10000003
+
+Input 2:
+    A = 10
+    B = 1
+    C = [1, 8, 11, 3]
+Output 2:
+    11
+*/
+
+// Binary Search on time
+
+const int MOD = 10000003;
+
+bool can(long long t, int A, vector<int> &C)
 {
-    long long n=C.size(), curr=C[0], sum=0;
-    for(long long i=0;i<n;++i)
+    int i = 0;
+    while(i < C.size())
     {
-        sum+=C[i];
-        if(sum>mid)
-        {
-            --i;
-            --A;
-            curr=C[i];
-            sum=0;
-        }
-        if(A==0)
-        {
+        if(C[i] > t || A == 0)
             return false;
+        
+        else
+        {
+            int tempt = t;
+            int j = i;
+            while(j < C.size() && tempt-C[j] >= 0)
+            {
+                tempt -= C[j];
+                ++j;
+            }
+            i = j;
+            A--;
         }
     }
     return true;
 }
 
-long long binary(int A, int B, vector<int> &C, int max, int sum)
+int Solution::paint(int A, int B, vector<int> &C) 
 {
-    long long l=0,r=sum,mid=0;
-    while(r-l>1)
+    long long maxEle = *max_element(C.begin(), C.end());
+    if(A >= C.size())
+        return (maxEle*B)%MOD;
+    
+    long long sum = 0;
+    for(int i = 0; i < C.size(); ++i)
+        sum += C[i];
+    
+    long long l = maxEle, r = sum, mid;
+    
+    while(r-l > 1)
     {
-        mid=(l+r)/2;
-        bool temp = can(A,B,C,mid);
+        mid = (l+r)/2;
+        bool temp = can(mid, A, C);
+        
         if(temp)
-        {
-            r=mid;
-        }
+            r = mid;
         else
-        {
-            l=mid+1;
-        }
+            l = mid;
     }
-    if(can(A,B,C,l))
-    {
-        return l%10000003;
-    }
-    return r%10000003;
-}
-
-int Solution::paint(int A, int B, vector<int> &C) {
-    long long n=C.size(),max=0,sum=0;
-    for(long long i=0;i<n;++i)
-    {
-        C[i]%=10000003;
-        sum+=C[i];
-        sum=sum%10000003;
-        if(C[i]>max)
-        {
-            max=C[i];
-        }
-    }
-    if(A>C.size())
-    {
-        return max*(B%10000003);
-    }
-    long long ans = binary(A,B,C,max,sum)*(B%10000003);
-    ans = ans%10000003;
-    return (int)ans;
+    if(can(l, A, C))
+        return (l*B)%MOD;
+    
+    return (r*B)%MOD;
 }
